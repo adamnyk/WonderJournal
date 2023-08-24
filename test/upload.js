83e2cded0a0b6fbe5a090d085789000774c9936d
Adam Pecan <adamnyk@gmail.com>
2023-08-24 15:25:19 -0600

    init

---
+++ test/upload.js
@@ -0,0 +1,24 @@
+const { PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+const fs = require("fs");
+const s3Client = new S3Client({});
+
+const bucketName = "wonder-journal";
+const fileName = "screenshot.png";
+const fileData = fs.readFileSync(fileName);
+
+
+s3Client.send(
+	new PutObjectCommand({
+		Bucket: bucketName,
+		Body: fileData,
+		Key: fileName,
+	})
+),
+	(err, data) => {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log(`File uploaded successfully. ${data.Location}`);
+		}
+	};
+
